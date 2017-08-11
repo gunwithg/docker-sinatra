76ee6f62b39acc360a5ca19065209eb2b85d7346
gunwithg <gunwithg@gmail.com>
2017-08-11 11:10:12 +0200

    deploy

---
+++ gitpush.sh
@@ -0,0 +1,13 @@
+#go to home and setup git  
+cd $HOME
+git config --global user.email "Iamtravis@travis.com"
+git config --global user.name "Iam Travis" 
+#clone the repository in the buildApk folder
+git clone --quiet --branch=master  https://gunwithg:$GITHUB_API_KEY@github.com/gunwithg/docker-sinatra  master > /dev/null
+#go into directory and copy data we're interested
+cd master  
+#add, commit and push files
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
+git push -fq origin deploy > /dev/null
+echo -e "Done\n"
