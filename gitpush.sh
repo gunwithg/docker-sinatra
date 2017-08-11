3268d59047d8cda28409c2ca771edd5b4f10b00c
gunwithg <gunwithg@gmail.com>
2017-08-11 11:38:24 +0200

    test 9

--- gitpush.sh
+++ gitpush.sh
@@ -3,10 +3,11 @@ cd $HOME
 git config --global user.email "Iamtravis@travis.com"
 git config --global user.name "Iam Travis" 
 #clone the repository in the buildApk folder
-git clone --quiet --branch=master  https://gunwithg:$GITHUB_API_KEY@github.com/gunwithg/docker-sinatra  master > /dev/null
+git clone --branch=master  https://gunwithg:$GITHUB_API_KEY@github.com/gunwithg/docker-sinatra  master > /dev/null
 #go into directory and copy data we're interested
 cd master  
 #add, commit and push files
+ git remote add origin https://gunwithg:$GITHUB_API_KEY@github.com/gunwithg/docker-sinatra.git
 touch projectname.hcl
 echo "${TRAVIS_BUILD_NUMBER}" > projectname.hcl
 git add -f .
