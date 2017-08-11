ab8bda728bf2d5912a0f770c837c28d1821ab04b
gunwithg <gunwithg@gmail.com>
2017-08-11 11:22:55 +0200

    test

--- gitpush.sh
+++ gitpush.sh
@@ -7,9 +7,9 @@ git clone --quiet --branch=master  https://gunwithg:$GITHUB_API_KEY@github.com/g
 #go into directory and copy data we're interested
 cd master  
 #add, commit and push files
-git add -f .
 touch projectname.hcl
 echo "${TRAVIS_BUILD_NUMBER}" > projectname.hcl
+git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
 git push -fq origin deploy > /dev/null
 echo -e "Done\n"
