bb6672ae566bf15bda4e50ff99c8ad561dc71e49
gunwithg <gunwithg@gmail.com>
2017-08-11 15:20:35 +0200

    test 22

--- gitpush.sh
+++ gitpush.sh
@@ -12,69 +12,68 @@ cd master
 ls -alih
 cat .git/config
 
-#touch projectname.hcl
-#echo "${TRAVIS_BUILD_NUMBER}" > projectname.hcl
+touch projectname.hcl
+echo "${TRAVIS_BUILD_NUMBER}" > projectname.hcl
 
 #NOMAD_JOB=$(basename `pwd`)
 #DOCKER_REPO=$1
 #DOCKER_TAG=$2
 
-cat > projectname.hcl << NOMADCONFIG
-job "NOMAD_JOB}" {
-  region      = "aws"
-  datacenters = ["eu-west-1"]
-  type = "service"
-
-  update {
-    stagger      = "60s"
-    max_parallel = 1
-  }
-
-  constraint {
-    attribute = "${node.class}"
-    value     = "Edge_Node"
-  }
-
-  group "${DOCKER_REPO}" {
-    count = 1
-
-    task "deploy" {
-      driver = "docker"
-      config {
-        image = "${DOCKER_REPO}:${DOCKER_TAG}"
-        force_pull = true
-        auth {
-          username =  "${DOCKER_USERNAME}"
-          password =  "${DOCKER_PASSWORD}"
-        }
-
-        port_map {
-          http = 80
-          management = 7777
-        }
-      }
-
-      # Specify the maximum resources required to run the job,
-      # include CPU, memory, and bandwidth.
-      resources {
-        cpu    = 1024 # MHz
-        memory = 1024 # MB
-
-        network {
-          mbits = 10
-          port "http" {
-            static = 80
-          }
-          port "management" {
-            static = 7777
-          }
-        }
-      }
-    }
-  }
-}
-NOMADCONFIG
-
+#cat > projectname.hcl << NOMADCONFIG
+#job "NOMAD_JOB}" {
+#  region      = "aws"
+#  datacenters = ["eu-west-1"]
+#  type = "service"
+#
+#  update {
+#    stagger      = "60s"
+#    max_parallel = 1
+#  }
+#
+#  constraint {
+#    attribute = "${node.class}"
+#    value     = "Edge_Node"
+#  }
+#
+#  group "${DOCKER_REPO}" {
+#    count = 1
+#
+#    task "deploy" {
+#      driver = "docker"
+#      config {
+#        image = "${DOCKER_REPO}:${DOCKER_TAG}"
+#        force_pull = true
+#        auth {
+#          username =  "${DOCKER_USERNAME}"
+#          password =  "${DOCKER_PASSWORD}"
+#        }
+#
+#        port_map {
+#          http = 80
+#          management = 7777
+#        }
+#      }
+#
+#      # Specify the maximum resources required to run the job,
+#      # include CPU, memory, and bandwidth.
+#      resources {
+#        cpu    = 1024 # MHz
+#        memory = 1024 # MB
+#
+#        network {
+#          mbits = 10
+#          port "http" {
+#            static = 80
+#          }
+#          port "management" {
+#            static = 7777
+#          }
+#        }
+#      }
+#    }
+#  }
+#}
+#NOMADCONFIG
 
 
 git add -f .
