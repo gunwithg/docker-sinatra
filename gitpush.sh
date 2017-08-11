6bcff771d04be0a81247354cea1434be0d0dc4eb
gunwithg <gunwithg@gmail.com>
2017-08-11 14:16:05 +0200

    test 15

--- gitpush.sh
+++ gitpush.sh
@@ -1,17 +1,73 @@
-#go to home and setup git  
 cd $HOME
-git config --global user.email "Iamtravis@travis.com"
 git config --global user.name "Iam Travis" 
-#clone the repository in the buildApk folder
+git config --global user.email "Iamtravis@travis.com"
 git clone --branch=master  https://gunwithg:$GITHUB_API_KEY@github.com/gunwithg/docker-sinatra  master > /dev/null
-#go into directory and copy data we're interested
+
 cd master  
 ls -alih
 cat .git/config
-#add, commit and push files
-#git remote add origin https://gunwithg:$GITHUB_API_KEY@github.com/gunwithg/docker-sinatra.git
-touch projectname.hcl
-echo "${TRAVIS_BUILD_NUMBER}" > projectname.hcl
+
+#touch projectname.hcl
+#echo "${TRAVIS_BUILD_NUMBER}" > projectname.hcl
+
+cat > projectname.hcl << NOMADCONFIG
+job "${DOCKER_REPO}" {
+  region      = "aws"
+  datacenters = ["eu-west-1"]
+  type = "service"
+
+  update {
+    stagger      = "60s"
+    max_parallel = 1
+  }
+
+  constraint {
+    attribute = "${node.class}"
+    value     = "Edge_Node"
+  }
+
+  group "${DOCKER_REPO}" {
+    count = 1
+
+    task "deploy" {
+      driver = "docker"
+      config {
+        image = "${DOCKER_REPO}:${DOCKER_TAG}"
+        force_pull = true
+        auth {
+          username =  "${DOCKER_USERNAME}"
+          password =  "${DOCKER_PASSWORD}"
+        }
+
+        port_map {
+          http = 80
+          management = 7777
+        }
+      }
+
+      # Specify the maximum resources required to run the job,
+      # include CPU, memory, and bandwidth.
+      resources {
+        cpu    = 1024 # MHz
+        memory = 1024 # MB
+
+        network {
+          mbits = 10
+          port "http" {
+            static = 80
+          }
+          port "management" {
+            static = 7777
+          }
+        }
+      }
+    }
+  }
+}
+NOMADCONFIG
+
+
+
 git add -f .
 git remote rm origin
 git remote add origin https://gunwithg:$GITHUB_API_KEY@github.com/gunwithg/docker-sinatra.git
@@ -19,3 +75,13 @@ cat .git/config
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
 git push -fq origin master:deploy 
 echo -e "Done\n"
+
+
+echo ${TRAVIS_BUILD_DIR}
+echo ${TRAVIS_BUILD_ID}
+echo ${TRAVIS_BUILD_NUMBER}
+echo ${TRAVIS_COMMIT}
+echo ${TRAVIS_COMMIT_MESSAGE}
+echo ${TRAVIS_COMMIT_RANGE}
+echo ${TRAVIS_EVENT_TYPE}
+echo ${TRAVIS_JOB_ID}
